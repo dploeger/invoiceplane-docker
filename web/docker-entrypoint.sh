233097ebc2386880f30e3bb145325076063d4113
Dennis Plöger <develop@dieploegers.de>
2019-01-03 11:31:24 +0100

    Addons-Support. Updated to ip v2.0.0 branch

--- web/docker-entrypoint.sh
+++ web/docker-entrypoint.sh
@@ -6,9 +6,10 @@ cd /var/www/html && php /composer.phar install --no-dev
 cd /var/www/html && npm install
 cd /var/www/html && grunt build
 
+cp -r /addons/* /var/www/html/custom/addons
+
 chmod -R 0777 /var/www/html/storage
 chmod -R 0777 /var/www/html/bootstrap/cache
-sed -i -re "s/localhost/mysql/gi" config/database.php
 
 echo "## InvoicePlane is ready! Starting Web server..."
 
